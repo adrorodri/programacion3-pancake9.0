8dcdbf2095df8929d5e22b04faf840263e22eb76
Michael <michael.velarde.kubber@gmail.com>
2018-04-16 09:03:51 -0800

    Login

--- app/src/main/java/com/programacion3/pancake9/app/MainActivity.java
+++ app/src/main/java/com/programacion3/pancake9/app/MainActivity.java
@@ -1,13 +1,15 @@
 package com.programacion3.pancake9.app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void clickbuttonL(View view ){
+        Intent     i = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(i);
     }
 }
