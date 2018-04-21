21ce15b3f42790bf0e8df050049d9da00bcfd704
Michael <michael.velarde.kubber@gmail.com>
2018-04-20 17:14:16 -0800

    sign up activity error corregido

--- app/src/main/java/com/example/andres/mundial/SignUpActivity.java
+++ app/src/main/java/com/example/andres/mundial/SignUpActivity.java
@@ -1,7 +1,9 @@
 package com.example.andres.mundial;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    public void click(View view) {
+        Intent i;
+        i = new Intent(this, TablaPartidos.class);
+        startActivity(i);
     }
 }
