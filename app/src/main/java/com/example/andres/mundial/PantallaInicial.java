2975e0c1c9845855f23b75d4fd0b194edd240f53
Christopher <ch.rash37@gmail.com>
2018-04-23 08:59:16 -0400

    Tabla partidos

---
+++ app/src/main/java/com/example/andres/mundial/PantallaInicial.java
@@ -0,0 +1,14 @@
+package com.example.andres.mundial;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PantallaInicial extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pantallainicial);
+
+    }
+}
