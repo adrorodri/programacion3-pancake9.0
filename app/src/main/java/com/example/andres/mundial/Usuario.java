eeff515c6aff8f451f5a51aba591d9f2145a6183
Michael <michael.velarde.kubber@gmail.com>
2018-04-23 14:18:28 -0800

    solucion pantalla inicial ( tablaPartidos)

--- app/src/main/java/com/example/andres/mundial/Usuario.java
+++ app/src/main/java/com/example/andres/mundial/Usuario.java
@@ -1,14 +1,13 @@
 package com.example.andres.mundial;
 
-import android.app.Activity;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class PantallaInicial extends AppCompatActivity{
+public class Usuario extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.pantallainicial);
-
+        setContentView(R.layout.activity_usuario);
     }
 }
