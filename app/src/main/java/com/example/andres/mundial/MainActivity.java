e1286f8553385870d353aa01f54863fb2423fc06
Gabriel Torrico M <pgtorricoo98@gmail.com>
2018-05-07 09:22:08 -0400

    g

--- app/src/main/java/com/example/andres/mundial/MainActivity.java
+++ app/src/main/java/com/example/andres/mundial/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.WindowManager;
 
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -13,6 +14,8 @@ public class MainActivity extends AppCompatActivity {;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
+
         setContentView(R.layout.activity_main);
 
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
